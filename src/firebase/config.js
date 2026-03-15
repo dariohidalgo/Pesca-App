416e0b4a3c7bfc6203ecc78841e23f436bd99b19
Dario Hidalgo <dhidalgo651@gmail.com>
2026-03-15 17:40:01 -0300

    refactor: Use environment variables for Firebase configuration.

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBpZ8wGHoSTThXKtLkRKILpakWyco1F0j8",
-  authDomain: "pesca-app-cordoba-pro.firebaseapp.com",
-  projectId: "pesca-app-cordoba-pro",
-  storageBucket: "pesca-app-cordoba-pro.firebasestorage.app",
-  messagingSenderId: "907542620530",
-  appId: "1:907542620530:web:5d0b401cfa5ddc244ad5eb"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
