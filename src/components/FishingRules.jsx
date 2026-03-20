a702d43ef58050b7a04a5887ce5a487b3eda74c6
Dario Hidalgo <dhidalgo651@gmail.com>
2026-03-20 08:21:52 -0300

    feat: add Fish icon import from lucide-react.

--- src/components/FishingRules.jsx
+++ src/components/FishingRules.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Helmet } from 'react-helmet-async';
-import { Scale, FileText, Anchor, Info, ExternalLink, CalendarDays, ShieldAlert } from 'lucide-react';
+import { Scale, FileText, Anchor, Info, ExternalLink, CalendarDays, ShieldAlert, Fish } from 'lucide-react';
 
 const fishingRules = [
     {
